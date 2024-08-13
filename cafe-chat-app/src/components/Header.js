981631e9cde66c1ecb765405196db12e7f732ef8
piyalak199 <128875508+piyalak199@users.noreply.github.com>
2024-08-13 17:58:06 +0700

    Revert "bua"

--- cafe-chat-app/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <div className="header">
-      <div class="container absolute inset-x-0 top-0">
-        <header class="bg-black p-4"></header>
-      </div>
-    </div>
-  );
-}
