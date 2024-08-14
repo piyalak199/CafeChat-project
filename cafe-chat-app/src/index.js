652f93ed7aa792567ad8f55ceb0d999e8eeb3b41
piyalak199 <piyalak4259@gmail.com>
2024-08-14 23:22:42 +0700

    ิbua

--- cafe-chat-app/src/index.js
+++ cafe-chat-app/src/index.js
@@ -35,7 +35,7 @@ import { AuthProvider } from "./AuthContext";
 import Login from "./Login";
 import Signup from "./Signup";
 import Home from "./Home";
-// import RoomList from "./RoomList";
+import RoomList from "./RoomList";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
       </AuthProvider>
     ),
   },
-  // { path: "/roomlist", element: <RoomList /> },
+  { path: "/roomlist", element: <RoomList /> },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
